6342c1cac7b3af0e4ae6d868131915b682aa4c45
Adkira 155 <adindaridha123@gmail.com>
2025-02-26 12:50:57 +0800

    logout admin

--- resources/views/livewire/layout/sidebar.blade.php
+++ resources/views/livewire/layout/sidebar.blade.php
@@ -1,4 +1,20 @@
+<?php
 
+use App\Livewire\Actions\Logout;
+use Livewire\Volt\Component;
+
+new class extends Component
+{
+    /**
+     * Log the current user out of the application.
+     */
+    public function logout(Logout $logout): void
+    {
+        $logout();
+
+        $this->redirect('/', navigate: true);
+    }
+}; ?>
 <div>
     <aside id="sidebar" class="h-screen w-64 bg-white shadow transition-all duration-300 z-40">
         <div class="flex flex-col h-full">
