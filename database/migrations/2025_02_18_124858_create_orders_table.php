d352188115bbea0620a88c1f6d491a48218fcb62
Adkira 155 <adindaridha123@gmail.com>
2025-04-09 08:46:48 +0800

    buck up

--- database/migrations/2025_02_18_124858_create_orders_table.php
+++ database/migrations/2025_02_18_124858_create_orders_table.php
@@ -38,7 +38,7 @@ return new class extends Migration
                 $table->string('midtrans_transaction_id')->nullable();
                 $table->string('midtrans_payment_type')->nullable();
                 $table->string('snap_token')->nullable();
-                $table->string('resi_code')->nullable();
+                // $table->string('resi_code')->nullable();
     
                 $table->string('invoice')->nullable();
                 $table->index('status');
